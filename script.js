4ceb9e4d5919945bd549565c58bcd36acd559611
imac1 <irina.macovei@yahoo.fr>
2022-08-03 10:04:46 +0300

    starter files

---
+++ script.js
@@ -0,0 +1,13 @@
+function switchTheme() {
+
+	// let menuItems = document.querySelectorAll('light');
+	const buttonTheme = document.getElementById("themeButton");
+	const elementCard = document.querySelectorAll(".card");
+	console.log(elementCard);
+	elementCard.forEach((element) => {
+		element.classList.toggle("bg-dark");
+		element.classList.toggle("text-white");
+	});
+
+}
+console.log(changeTheme());
